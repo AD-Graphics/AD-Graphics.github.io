5c53432ac01352a9599c8ffae908cc69fcd5b130
AD-Graphics <104602552+AD-Graphics@users.noreply.github.com>
2022-06-14 21:33:07 -0400

    js_slideshow

---
+++ js/slideshow.js
@@ -0,0 +1,40 @@
+let slide1 = document.getElementById("slide1");
+let slide2 = document.getElementById("slide2");
+let slide3 = document.getElementById("slide3");
+let slide4 = document.getElementById("slide4");
+
+let slides = [slide1, slide2, slide3, slide4];
+
+let currentSlideIndex = 0;
+
+function showCurrentSlide() {
+  if (currentSlideIndex < 0) {
+    currentSlideIndex = slides.length - 1;
+  }
+
+  if (currentSlideIndex >= slides.length) {
+    currentSlideIndex = 0;
+  }
+  
+  for (let i = 0; i < slides.length; i++) {
+    let loopSlide = slides[i];
+    // DOES NOT EQUAL
+    if (i != currentSlideIndex) {
+      loopSlide.style.display = "none";
+    } else {
+      loopSlide.style.display = "inline-block";
+    }
+  }
+}
+
+showCurrentSlide();
+
+function showNextSlide() {
+  currentSlideIndex++;
+  showCurrentSlide();
+}
+
+function showPreviousSlide() {
+  currentSlideIndex--;
+  showCurrentSlide();
+}
